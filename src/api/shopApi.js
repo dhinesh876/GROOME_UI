f82643f3d0519e60e24e995fe0655ff01c8a1e60
Dhineshkumar P <dhinesh876@gmail.com>
2026-07-24 00:29:30 +0530

    Shop owner backend connection with ui design - 1

--- src/api/shopApi.js
+++ src/api/shopApi.js
@@ -6,8 +6,8 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
-const Authrefresh = "http://localhost:3000/auth";
+const BASE_URL = "https://groome-backend.onrender.com";
+const Authrefresh = "https://groome-backend.onrender.com/auth";
 
 const api = axios.create({
   baseURL: `${BASE_URL}/shop`,
